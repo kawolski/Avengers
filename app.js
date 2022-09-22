fb5d80ba94c7e8e6a23453251d598d8048239fe8
Prashant Rawat <prashantxrawat@gmail.com>
2022-09-22 12:54:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('.................BelcOm ABhenJors....................')
+  res.send('.................BelcOm OPtional Port ABhenJors....................')
 })
 
 app.listen(port, () => {
